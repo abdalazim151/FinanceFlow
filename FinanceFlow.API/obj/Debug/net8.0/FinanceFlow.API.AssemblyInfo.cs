de01bb9b8ff5a966ef6177cc3132c16ccb1e517c
abdalazim151 <abdalazim.ahmed11111@gmail.com>
2026-03-09 16:49:33 +0200

    Refine CreateAtm endpoint implementation in OperationController to enhance ATM creation process

--- FinanceFlow.API/obj/Debug/net8.0/FinanceFlow.API.AssemblyInfo.cs
+++ FinanceFlow.API/obj/Debug/net8.0/FinanceFlow.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceFlow.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b7211b033dbe5a4e0390aff16bd6d41327a1ed9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa3f6f1e5131138bea3fb857b5ee922d8e6c00f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceFlow.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceFlow.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
