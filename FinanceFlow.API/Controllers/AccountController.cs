93c44223509ea2b8dff21b8150bb2f505a754c7b
abdalazim151 <abdalazim.ahmed11111@gmail.com>
2026-03-22 18:11:29 +0200

    update Account controller

--- FinanceFlow.API/Controllers/AccountController.cs
+++ FinanceFlow.API/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 using FinanceFlow.Application.Common.DTOs;
 using FinanceFlow.Application.Features.Authentication.Commands;
+using FinanceFlow.Infrastructure.Serivces;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -12,10 +13,16 @@ namespace FinanceFlow.API.Controllers
     public class AccountController : ControllerBase
     {
         private IMediator mediator;
+        private EmailService emailService;
+        private readonly ReportService reportService;
 
-        public AccountController(IMediator mediator)
+        public AccountController(IMediator mediator
+            , EmailService service
+            ,ReportService reportService)
         {
             this.mediator = mediator;
+            this.emailService = service;
+            this.reportService = reportService;
         }
 
         [AllowAnonymous]
@@ -36,6 +43,30 @@ namespace FinanceFlow.API.Controllers
             if (res.IsSuccess)
                 return Ok(res);
             return BadRequest(res);
+        }
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<ActionResult<bool>> TestEmail()
+        {
+            try
+            {
+
+                emailService.Send("abdalazim.ahmed20@gmail.com", "test", "test,test");
+                return Ok(true);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            return Ok();
+        }
+        [AllowAnonymous]
+        [HttpGet("TestReport")]
+        public async Task<ActionResult> testReport()
+        {
+            await reportService.Generate("Feed","abdalazim.ahmed20@gmail.com");
+            return Ok(true);
+
         }
     }
 }
